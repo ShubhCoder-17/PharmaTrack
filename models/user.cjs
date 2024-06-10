02ba778f70ec96e1fcb7b7b62b95f04325d08e82
ShubhCoder-17 <1705patrashubhadeep@gmail.com>
2024-06-10 13:50:31 +0530

    new code added;

--- models/user.cjs
+++ models/user.cjs
@@ -1,13 +1,39 @@
-// models/user.cjs
-const { DataTypes } = require('sequelize');
-const sequelize = require('./index.cjs'); // Ensure the correct path
+// // models/user.cjs
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('./index.cjs'); // Ensure the correct path
+
+// const User = sequelize.define('User', {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     primaryKey: true,
+//     autoIncrement: true,
+//   },
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   email: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//     unique: true,
+//   },
+//   password: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+// }, {
+//   tableName: 'users', // Name of the table in the database
+//   timestamps: false, // Disable timestamps if not used
+// });
+
+// module.exports = User;
+
+
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('./index.cjs');
+const bcrypt = require('bcryptjs');
 
 const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
   name: {
     type: DataTypes.STRING,
     allowNull: false,
@@ -22,8 +48,24 @@ const User = sequelize.define('User', {
     allowNull: false,
   },
 }, {
-  tableName: 'users', // Name of the table in the database
-  timestamps: false, // Disable timestamps if not used
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+    beforeUpdate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+  }
 });
+
+User.prototype.validatePassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = User;
